861483af32bf456867178d761ef39012a4a3bd49
Mohammed Salih v <infosalihv@gmail.com>
2025-05-22 12:18:13 +0530

    some bug fixes and improvement

--- client/src/routes/Protect/PublicOnlyRoute.tsx
+++ client/src/routes/Protect/PublicOnlyRoute.tsx
@@ -1,4 +1,4 @@
-// src/routes/Protect/PublicOnlyRoute.tsx
+
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { RootState } from "@/redux/store";
@@ -9,9 +9,8 @@ interface Props {
 
 const PublicOnlyRoute: React.FC<Props> = ({ children }) => {
   const userInfo = useSelector((state: RootState) => state.user.userInfo);
-  const hostInfo = useSelector((state: RootState) => state.host.hostInfo);
 
-  const isAuthenticated = userInfo?.accessToken || hostInfo?.accessToken;
+  const isAuthenticated = userInfo?.accessToken;
 
   if (isAuthenticated) {
     return <Navigate to="/user/home" replace />;
